58474ee751de1f588c0a4d3920d7569c2e6b9f29
ASoldo <soldo.andrej@gmail.com>
2024-02-06 18:44:22 +0100

    Refactor: Remove SurrealData and use static Lazy for SurrealDB direct access
    Since SurrealDB is "sync" and "send" it is safe to clone it and share it therefore no need for Mutex since SurrealDB solved this issue for us.
    Note:
    SurrealDB devs confirmed this

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use once_cell::sync::Lazy;
 use serde::Serialize;
 use serde_json::Value;
-use std::sync::Mutex;
 use surrealdb::engine::remote::ws::{Client, Ws};
 use surrealdb::opt::auth::Root;
 use surrealdb::{Error, Surreal};
@@ -13,44 +13,35 @@ struct Person {
     marketing: bool,
 }
 
-struct SurrealData {
-    db: Mutex<Surreal<Client>>,
-}
-
+static DB: Lazy<Surreal<Client>> = Lazy::new(|| Surreal::init());
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting Actix server on http://127.0.0.1:8080");
-
-    let db = match Surreal::new::<Ws>("0.0.0.0:8000").await {
-        Ok(db) => db,
-        Err(e) => {
-            eprintln!("Failed to connect to the database: {}", e);
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Database connection failed",
-            ));
-        }
-    };
-
-    if let Err(e) = db
-        .signin(Root {
-            username: "root",
-            password: "root",
-        })
+    println!("Connecting to the database...");
+    DB.connect::<Ws>("127.0.0.1:8000")
         .await
-    {
-        eprintln!("Failed to sign in to the database: {}", e);
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Database signin failed",
-        ));
-    }
+        .expect("Unnable to connect to the database");
+    println!("Connected!");
 
-    let db = web::Data::new(SurrealData { db: Mutex::new(db) });
+    println!("Signing in to the database...");
+    DB.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await
+    .expect("Unnable to sign in to the database");
+    println!("Signed in!");
 
+    println!("Setting up the namespace and database...");
+
+    DB.use_ns("test")
+        .use_db("test")
+        .await
+        .expect("Unnable to select namespace/database");
+    println!("Setup complete!");
+
+    println!("Starting Actix server on http://127.0.0.1:8080");
     HttpServer::new(move || {
         App::new()
-            .app_data(db.clone())
             .service(index)
             .service(insert_person)
             .service(query_person)
@@ -66,27 +57,14 @@ async fn index() -> impl Responder {
 }
 
 #[get("/insert_person")]
-async fn insert_person(db: web::Data<SurrealData>) -> impl Responder {
-    let db_lock = match db.db.lock() {
-        Ok(lock) => lock,
-        Err(_) => {
-            return HttpResponse::ServiceUnavailable()
-                .body("Service is temporarily unavailable. Please try again later.")
-        }
-    };
-
-    if let Err(e) = db_lock.use_ns("test").use_db("test").await {
-        return HttpResponse::InternalServerError()
-            .body(format!("Error selecting namespace/database: {}", e));
-    }
-
+async fn insert_person() -> impl Responder {
     let person = Person {
         title: "Founder & CEO".to_string(),
         name: "Rootster".to_string(),
         marketing: true,
     };
 
-    let result: Result<Vec<Value>, Error> = db_lock.create("person").content(&person).await;
+    let result: Result<Vec<Value>, Error> = DB.create("person").content(&person).await;
 
     match result {
         Ok(response) => HttpResponse::Ok().json(response),
@@ -97,21 +75,8 @@ async fn insert_person(db: web::Data<SurrealData>) -> impl Responder {
 }
 
 #[get("/query_person")]
-async fn query_person(db: web::Data<SurrealData>) -> impl Responder {
-    let db_lock = match db.db.lock() {
-        Ok(lock) => lock,
-        Err(_) => {
-            return HttpResponse::ServiceUnavailable()
-                .body("Service is temporarily unavailable. Please try again later.")
-        }
-    };
-
-    if let Err(e) = db_lock.use_ns("test").use_db("test").await {
-        return HttpResponse::InternalServerError()
-            .body(format!("Error selecting namespace/database: {}", e));
-    }
-
-    let result: Result<Vec<Value>, Error> = db_lock.select("person").await;
+async fn query_person() -> impl Responder {
+    let result: Result<Vec<Value>, Error> = DB.select("person").await;
 
     match result {
         Ok(records) => HttpResponse::Ok().json(records),
